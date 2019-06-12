518d844c120dda4bb45accf60a183d09847c4ed7
Andrew Fox <andrewbfox@gmail.com>
2019-06-12 17:27:53 -0400

    modified prototypes.js sketching out GameObject, CharacterStats, and Humanoid constructor functions

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -39,6 +39,34 @@
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;        // looks like we need a Date() constructor function
+  this.name = attributes.name;                  // string
+  this.dimensions = attributes.dimensions       // object {length, width, height}
+  this.destroy = function() {                   // prototype method
+    return `${this.name} was removed from the game.`
+  };
+}
+
+function CharacterStats(attributes) {
+  this.healthPoints = attributes.healthPoints;  // number
+  this.takeDamage = function() {                // prototype method
+    return `${this.name} took damage.`
+  };
+  // inherit destroy() from GameObject
+}
+
+function Humanoid(attributes) {
+  this.team = attributes.team;                  // string
+  this.weapons = attibutes.weapons;             // string array
+  this.langauge = attributes.language;          // string
+  this.greet = function() {                     // prototype method
+    return `${this.name} offers a greeting in ${this.langauge}.`
+  };
+  // inherit destroy() from GameObject through CharacterStats
+  // inherit takeDamage() from CharacterStats
+}
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
 /*
